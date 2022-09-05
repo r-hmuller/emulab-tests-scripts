711f0154433c4e6e91d150e17e80570c01ed60a4
Rodrigo Müller <rodrigo.muller@petlove.com.br>
2022-09-05 19:33:00 -0300

    Create docker-compose for registry server

--- script-secondary.sh
+++ script-secondary.sh
@@ -7,7 +7,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin apache2-utils -y
 snap install microk8s --classic
 microk8s status --wait-ready
 
